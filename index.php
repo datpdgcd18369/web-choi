89e1aab6bf7cb14377b17e5ca78f39a6c6060eb4
dat <amidateri@gmail.com>
2020-05-26 17:16:30 +0700

    asdasda

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1> chao</h1>
+        <?php
+       
+        ?>
+    </body>
+</html>
